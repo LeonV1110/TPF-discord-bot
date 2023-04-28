a692ddddad26036272b0e3f7b35ce624c15f43ad
LeonV1110 <59734771+LeonV1110@users.noreply.github.com>
2023-04-28 21:31:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -279,7 +279,9 @@ async def setup_database(inter):
 async def get_role_ids(inter):
     await inter.response.defer()
     roles = inter.author.roles
+    res = ""
     for role in roles:
+        res += role + " : " + role.id + "\n"
         print(role)
         print(role.id)
     await inter.followup.send(embed = Embed(title= "Boo"))
