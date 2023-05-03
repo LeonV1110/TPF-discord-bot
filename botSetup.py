0b630f2bab374a687ded1488bd5acbbd49eedb6e
LeonVerhaegh <leon.l.verhaegh@gmail.com>
2023-05-03 12:33:18 +0200

    fix ?

--- botSetup.py
+++ botSetup.py
@@ -4,15 +4,16 @@ from disnake import Intents
 
 
 class PersistentBot(commands.bot):
-    def __init__(self, intents, command_prefix):
-        super().__init__(intents = intents, command_prefix=command_prefix)
+    def __init__(self):
+        intents = Intents.default()
+        intents.members = True
+        intents.message_content = True #TODO, probably not needed
+        super().__init__(intents = intents, command_prefix='/')
         self.persistent_views_added = False
     
 
 
 
 
-intents = Intents.default()
-intents.members = True
-intents.message_content = True #TODO, probably not needed
-bot = PersistentBot(intents = intents, command_prefix='/')
+
+bot = PersistentBot()
