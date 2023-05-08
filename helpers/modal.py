5af639dfe7913eaf1ce9e058a0e68b4bcc1f4ccd
LeonVerhaegh <leon.l.verhaegh@gmail.com>
2023-05-08 23:08:33 +0200

    Update modal.py

--- helpers/modal.py
+++ helpers/modal.py
@@ -7,10 +7,10 @@ from disnake.ui import Modal, TextInput
 class RegisterModal(Modal):
     def __init__(self, inter_id):
         components = [TextInput(label= 'Steam64ID', placeholder='76561198029817168', custom_id=inter_id, style=TextInputStyle.short, max_length=19)]
-        print('AARDAPPEL')
         super().__init__(title='Register', components=components, custom_id=inter_id, timeout=600)
     
     async def callback(self, inter: ModalInteraction):
+        print('AARDAPPEL')
         embed = disnake.Embed(title='Registration')
         for key, value in inter.text_values.items():
             print('Modal: Key: ' + str(key) + ' Value: ' + str(value))
