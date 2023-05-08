e0a8740aa2137eeb9ac24a90c9e1ac22d09b3beb
LeonVerhaegh <leon.l.verhaegh@gmail.com>
2023-05-08 23:41:47 +0200

    Implemented register modal

--- helpers/modal.py
+++ helpers/modal.py
@@ -3,12 +3,14 @@ from disnake.ext import commands
 from disnake import TextInputStyle
 from disnake.interactions.modal import ModalInteraction
 from disnake.ui import Modal, TextInput
-
+from helpers.botHelper import register_player
+from helpers.error import MyException
+from pymysql import OperationalError
 class RegisterModal(Modal):
     def __init__(self, inter_id):
         components = [
             TextInput(
-            label= 'Steam64ID', 
+            label= 'Please provide your Steam64ID for registration.', 
             placeholder='76561198029817168', 
             custom_id=str(inter_id), 
             style=TextInputStyle.short, 
@@ -16,12 +18,17 @@ class RegisterModal(Modal):
         super().__init__(title='Register', components=components, custom_id=str(inter_id), timeout=600)
     
     async def callback(self, inter: ModalInteraction):
-        print('AARDAPPEL')
         embed = disnake.Embed(title='Registration')
-        for key, value in inter.text_values.items():
-            print('Modal: Key: ' + str(key) + ' Value: ' + str(value))
-            pass #Do registration
-        await inter.response.send_message(embed=embed)
+        response = inter.text_values.items()
+        key, value = response[0]
+        try:
+            register_player(member=inter.author, steam64ID=value)
+        except MyException as error:
+            embed = disnake.Embed(title=error.message)
+        except OperationalError:
+            embed = disnake.Embed(
+            title="The bot is currently having issues, please try again later.")
+        await inter.response.send_message(embed=embed, ephemeral=True)
 
     async def on_error(self, error: Exception, inter: ModalInteraction):
         await inter.response.send_message(error)
